91a99995e4ac00696298fcf0fc13d72c0c9bacfe
anilbeesetti <anilbeesetti10@gmail.com>
2022-04-24 09:38:54 +0530

    failing UI test to find no videos found text

---
+++ app/src/androidTest/java/com/arcticoss/nxtplayer/medialistscreen/MediaListRobot.kt
@@ -0,0 +1,36 @@
+package com.arcticoss.nxtplayer.medialistscreen
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.AndroidComposeTestRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import com.arcticoss.nxtplayer.MainActivity
+import com.arcticoss.nxtplayer.R
+
+
+fun launchMediaListScreen(
+    rule: AndroidComposeTestRule<ActivityScenarioRule<MainActivity>, MainActivity>,
+    block: MediaListRobot.() -> Unit
+): MediaListRobot {
+    return MediaListRobot(rule).apply(block)
+}
+
+class MediaListRobot(
+    private val rule: AndroidComposeTestRule<ActivityScenarioRule<MainActivity>, MainActivity>
+) {
+
+    infix fun verify(
+        block: MediaListVerification.() -> Unit
+    ): MediaListVerification {
+        return MediaListVerification(rule).apply(block)
+    }
+}
+
+class MediaListVerification(
+    private val rule: AndroidComposeTestRule<ActivityScenarioRule<MainActivity>, MainActivity>
+) {
+    fun noVideoFilesFoundIsShown() {
+        val noVideoFiles = rule.activity.getString(R.string.noVideoFilesFound)
+        rule.onNodeWithText(noVideoFiles).assertIsDisplayed()
+    }
+}
---
+++ app/src/androidTest/java/com/arcticoss/nxtplayer/medialistscreen/MediaListScreenTest.kt
@@ -0,0 +1,22 @@
+package com.arcticoss.nxtplayer.medialistscreen
+
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import com.arcticoss.nxtplayer.MainActivity
+import org.junit.Rule
+import org.junit.Test
+
+class MediaListScreenTest {
+
+    @get:Rule
+    val videoListRule = createAndroidComposeRule<MainActivity>()
+
+    @Test
+    fun displayNoVideoFilesFound() {
+        launchMediaListScreen(videoListRule) {
+            // no operation
+        } verify {
+            noVideoFilesFoundIsShown()
+        }
+    }
+
+}
